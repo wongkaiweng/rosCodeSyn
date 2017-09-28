8df8d8dda51a9f5c766b380b65d376aedda63b50
Corinne Kenwood <corinnekenwood@Corinnes-MacBook-Pro.local>
2017-09-28 02:16:04 -0400

    Add check_limits file with testing examples

--- testing/check_limits.py
+++ testing/check_limits.py
@@ -5,11 +5,6 @@ import sys
 from urdf_parser_py.urdf import URDF
 import argparse
 
-#parse input parameters
-file=sys.argv[1] #URDF file with parameters of the robot
-comm=sys.argv[2] #command to be checked
-name=sys.argv[3] #name of joint command is being sent to
-
 #extract the limits of all the robot joints from the URDF file into a dictionary
 def make_dic(file):
 	robot = URDF.from_xml_file(file)
@@ -19,11 +14,11 @@ def make_dic(file):
 	for joint in robot.joints:
 		#add joints to dictionary dic with key=joint.name, value=joint.limit
 		dic[joint.name]=joint.limit
+	return dic
 
 #check if the input command is within the upper and lower limits of the joint
 #returns True if the command is within the limits and False if it is not
 def check_limits(dic,comm,name):
-
 	joint=dic[name] #get the joint from the dictionary
 
 	comm=float(comm) #convert from string
@@ -36,15 +31,41 @@ def check_limits(dic,comm,name):
 
 if __name__ == '__main__':
 	#parse input arguments
-	#parser=argparse.ArgumentParser(description="find input parameters")
-	#parser.add_argument('--file',type=str,help='Specify the name of the URDF or YAML file for the robot',nargs='?')
-	#parser.add_argument('--comm',type=str,help='Specify the desired command to be sent to the robot',nargs='?')
-	#parser.add_argument('--name',type=str,help='Specify the name of the joint to which the command is to be sent',nargs='?')
-	
-	#create a dictionary for the robot--ADD if yaml,elif urdf
+	parser=argparse.ArgumentParser(description="find input parameters")
+	parser.add_argument('--file',type=str,help='Specify the name of the URDF or YAML file for the robot',nargs='?')
+	parser.add_argument('--comm',type=str,help='Specify the desired command to be sent to the robot',nargs='?')
+	parser.add_argument('--name',type=str,help='Specify the name of the joint to which the command is to be sent',nargs='?')
+	args=parser.parse_args()
+
+	#example--sucess
+	file="jaco_arm.urdf"
+	comm="3"
+	name="jaco_joint_base"
+	print "Sending command of magnitude 3 to jaco_joint_base joint of jaco_arm."
+
+	#create a dictionary for the robot
 	dic=make_dic(file)
 	#check the desired command
 	check=check_limits(dic,comm,name)
-	print check
+	if check==True:
+		print "RESULT: The command is within the limits."
+	elif check==False:
+		print "RESULT: The command cannot be sent to the given joint."
 
-	#add example
+	#example--failure
+	file="jaco_arm.urdf"
+	comm="10"
+	name="jaco_joint_base"
+	print "Sending command of magnitude 10 to jaco_joint_base joint of jaco_arm."
+
+	#create a dictionary for the robot
+	dic=make_dic(file)
+	#check the desired command
+	check=check_limits(dic,comm,name)
+	if check==True:
+		print "RESULT: The command is within the limits."
+	elif check==False:
+		print "RESULT: The command cannot be sent to the given joint."
+
+
+
