72463b644a87ef2b742041da530aab3ca37c232a
Catherine Wong <wongkaiweng@gmail.com>
2017-12-19 17:07:13 -0500

    Added new version of code_synthesis.py ----------------------------------------------------------------- - Added scaling capability of velocity commands from source robot   to target robot - Restructured the file with functions for cleaner use - Added and cleaned up example from jackal to turtlebot

--- testing/code_synthesis.py
+++ testing/code_synthesis.py
@@ -7,6 +7,8 @@ import prob_from_files
 import parameters_in_file
 import replacement_with_redbaron
 import process_urdf
+import process_yaml
+import process_limits
 
 import logging_config
 synthesis_logger = logging.getLogger("synthesis_logger")
@@ -44,29 +46,164 @@ def topic_replacement_by_distribution(file_path, red_obj):
 
     return red_obj
 
+def convert_joint_traj_commands(red_obj, ast_file, source_robot_name, target_robot_name):
+    """
+    Convert source joint commands to target joint commands with
+    kinematic retargeting
+    red_obj: redbardon obj of source code
+    ast_file, souce code parsed by ast
+    source_robot_name: name of the original robot
+    target_robot_name: name of the target robot
+    """
+
+    rv = parameters_in_file.ROSParameterVisitor(['trajectory_msgs.msg.JointTrajectory','JointTrajectory'],\
+                                                 limits_dict={}, msg_fields_dict={})
+    rv.visit(ast_file)
+
+    # Find new joints with kdl retargetor
+    # joint_names
+    # points, positions
+    synthesis_logger.debug("All variables: {0}".format(rv.scopes[0]))
+    synthesis_logger.debug("Parameters of interest: {0}".format(rv.msg_fields_dict))
+
+    if 'joint_names' in rv.msg_fields_dict.keys():
+        for joint_names in rv.msg_fields_dict['joint_names']:
+
+            if 'points' in rv.msg_fields_dict.keys() and \
+               'positions' in rv.msg_fields_dict['points'].keys():
+
+                # get values of joint_names
+                if isinstance(joint_names,str):
+                    joints_names_values = rv.scopes[0].find(joint_names)
+                else:
+                    joints_names_values = joint_names
+
+                # if we have both joint_name fields and points/positions field
+                for positions in rv.msg_fields_dict['points']['positions']:
+
+                    # get values of positions
+                    if isinstance(positions,str):
+                        positions_values = rv.scopes[0].find(positions)
+                    else:
+                        positions_values = positions
+
+                    # ensure joints_names_values and positions_values are the same length
+                    if len(joints_names_values) == len(positions_values):
+                        synthesis_logger.info("================ Joint Trajectory Replacement ================")
+                        synthesis_logger.info("joints_names_values:{0}".format(joints_names_values))
+                        synthesis_logger.info("positions_values:{0}".format(positions_values))
+
+                        for joint_list in joints_names_values:
+                            for pos_list in positions_values:
+                                # do optimization with kdl_retargeter
+                                # E.g: joints_names_values:[['j2n6s300_joint_1', 'j2n6s300_joint_2', 'j2n6s300_joint_3',\
+                                #                       'j2n6s300_joint_4', 'j2n6s300_joint_5', 'j2n6s300_joint_6']]
+                                # E.g: positions_values:[[2.2, 2.3, 1.57, 0, 0, 0]]
+
+                                # ++++++++++++++++++++++++++++++++++++++++ #
+                                # ++++++++ MAKE IT MODULAR HERE ++++++++++ #
+                                # ++++++++++++++++++++++++++++++++++++++++ #
+                                target_joints, best_ret_angles = process_urdf.find_ret_angles_from_source_joints(\
+                                    source_robot_name, target_robot_name, joint_list, pos_list, mode='scale_by_length')
+
+                                # replace joint names
+                                #[attribute_list, limit_violation, limit, cur_value, var_name]
+                                replacement_list = [(['joint_names'], '--', \
+                                                    target_joints, joint_list, joint_names)]
+                                replacement_with_redbaron.replace_parameters(red_obj, replacement_list)
+
+                                # replace joint values
+                                replacement_list = [(['points','positions'], '--', \
+                                                    best_ret_angles, pos_list, positions)]
+                                replacement_with_redbaron.replace_parameters(red_obj, replacement_list)
+
+
+def convert_velocity_commands(red_obj, ast_file, source_robot_name, target_robot_name):
+    """
+    convert velocity commands of source robot to target robot's with scaling
+    red_obj: redbardon obj of source code
+    ast_file, souce code parsed by ast
+    source_robot_name: name of the original robot
+    target_robot_name: name of the target robot
+    """
+    # load velocity limits
+    source_vel_lim_dict =  process_limits.make_vel_limit_dic_from_yaml_text(process_yaml.find_robot_YAML(source_robot_name))
+    target_vel_lim_dict =  process_limits.make_vel_limit_dic_from_yaml_text(process_yaml.find_robot_YAML(target_robot_name))
+
+    # TODO: replace hard-coded list with our source_vel_limit_dict
+
+    # find parameters
+    rv = parameters_in_file.ROSParameterVisitor(['geometry_msgs.msg.Twist','Twist'], limits_dict=source_vel_lim_dict)
+    rv.visit(ast_file)
+
+    synthesis_logger.debug("All variables: {0}".format(rv.scopes[0]))
+    synthesis_logger.debug("Parameters of interest: {0}".format(rv.msg_fields_dict))
+
+    # scale velocity
+    # e.g.: rv.scopes[0]: {'turn': [0, 0], 'vel': [0, 0.3], 'target': [1.0]}
+    # e.g.:rv.msg_fields_dict: {'linear': {'x': ['vel']}, 'angular': {'z': ['turn']}}
+    for vel_type, vel_dir_dict in rv.msg_fields_dict.iteritems():
+        for vel_dir, field_list in vel_dir_dict.iteritems():
+            for param in field_list:
+                # get values of param
+                if isinstance(param,str):
+                    param_value_list = rv.scopes[0].find(param)
+                else:
+                    param_value_list = [param]
+
+                # check and scale
+                for param_value in param_value_list:
+                    scaled_value, scale_result = process_yaml.scale_velocity_command(\
+                                [vel_type, vel_dir], param_value, source_vel_lim_dict, target_vel_lim_dict)
+
+                    if scale_result:
+                        # replace twist values
+                        # [attribute_list, limit_violation, limit, cur_value, var_name]
+                        replacement_list = [([vel_type, vel_dir], '--', \
+                                            scaled_value, param_value, param)]
+                        replacement_with_redbaron.replace_parameters(red_obj, replacement_list)
+
+
+
 if __name__ == "__main__":
+    UR5_TO_JACO_EXAMPLE = False
+    MOVE_ROBOT_EXAMPLE = True
+
     TOPIC_REPLACEMENT = True
     PARAMETER_REPLACEMENT = True
 
-    #if sys.platform == 'darwin': # Mac OS
-    #    file_path = '/Users/{0}/Dropbox/ros_examples/turtlebot/processed/'.format(getpass.getuser())
-    #else: # linux /windows?
-    #    file_path = '/home/{0}/ros_examples/turtlebot/processed/'.format(getpass.getuser())
-    #filename = "files/jackal_auto_drive.py"
-    #filename = "files/wander.py"
-    #filename = "files/move_robot_jaco.py"
-    #filename = "files/set_velocity.py"
-    #dest_file = "files/set_velocity_mod.py"
+    # _____                           _
+    #| ____|_  ____ _ _ __ ___  _ __ | | ___  ___
+    #|  _| \ \/ / _` | '_ ` _ \| '_ \| |/ _ \/ __|
+    #| |___ >  < (_| | | | | | | |_) | |  __/\__ \
+    #|_____/_/\_\__,_|_| |_| |_| .__/|_|\___||___/
+    #                          |_|
 
-    # joint trajectory example (test_move - ur5 to jaco)
-    filename = "files/test_move_ur5.py"
-    dest_file = "files/code.py"
-    source_robot_name = 'ur5'
-    target_robot_name = 'kinova'
-    if sys.platform == 'darwin': # Mac OS
-        file_path = '/Users/{0}/Dropbox/ros_examples/jaco/semiprocessed/'.format(getpass.getuser())
-    else: # linux /windows?
-        file_path = '/home/{0}/ros_examples/jaco/semiprocessed/'.format(getpass.getuser())
+    if MOVE_ROBOT_EXAMPLE:
+        # (jackal to turtlebot)
+        #filename = "files/jackal_auto_drive.py"
+        filename = "files/wander.py"
+        #filename = "files/move_robot_jaco.py"
+        #filename = "files/set_velocity.py"
+        dest_file = "files/code.py"
+        source_robot_name = 'jackal'
+        target_robot_name = 'turtlebot'
+        if sys.platform == 'darwin': # Mac OS
+            file_path = '/Users/{0}/Dropbox/ros_examples/turtlebot/processed/'.format(getpass.getuser())
+        else: # linux /windows?
+            file_path = '/home/{0}/ros_examples/turtlebot/processed/'.format(getpass.getuser())
+
+    if UR5_TO_JACO_EXAMPLE:
+        # joint trajectory example (test_move - ur5 to jaco)
+        filename = "files/test_move_ur5.py"
+        dest_file = "files/code.py"
+        source_robot_name = 'ur5'
+        target_robot_name = 'kinova'
+        if sys.platform == 'darwin': # Mac OS
+            file_path = '/Users/{0}/Dropbox/ros_examples/jaco/semiprocessed/'.format(getpass.getuser())
+        else: # linux /windows?
+            file_path = '/home/{0}/ros_examples/jaco/semiprocessed/'.format(getpass.getuser())
+
     #  ____          _        ____              _   _               _
     # / ___|___   __| | ___  / ___| _   _ _ __ | |_| |__   ___  ___(_)___
     #| |   / _ \ / _` |/ _ \ \___ \| | | | '_ \| __| '_ \ / _ \/ __| / __|
@@ -103,13 +240,6 @@ if __name__ == "__main__":
         synthesis_logger.info("|_|   \__,_|_|  \__,_| |_| \_\___| .__/|_|\__,_|\___\___|_| |_| |_|\___|_| |_|\__|")
         synthesis_logger.info("                                 |_|                                              ")
 
-        vel_limits = {'linear':{'x':{'lower': -0.2, 'upper':0.2}, \
-                                'y':{'lower': -0.2, 'upper':0.2}, \
-                                'z':{'lower': -0.2, 'upper':0.2}},\
-                      'angular':{'x':{'lower': -0.2, 'upper':0.2}, \
-                                 'y':{'lower': -0.2, 'upper':0.2}, \
-                                 'z':{'lower': -0.2, 'upper':0.2}}}
-
         with open(filename) as f:
             ast_file = ast.parse(f.read())
 
@@ -119,11 +249,7 @@ if __name__ == "__main__":
         #  | |  \ V  V /| \__ \ |_
         #  |_|   \_/\_/ |_|___/\__|
 
-        rv = parameters_in_file.ROSParameterVisitor(['geometry_msgs.msg.Twist','Twist'], limits_dict=vel_limits)
-        rv.visit(ast_file)
-
-        if rv.out_of_bound_list:
-            replacement_with_redbaron.replace_parameters(red_obj, rv.out_of_bound_list)
+        convert_velocity_commands(red_obj, ast_file, source_robot_name, target_robot_name)
 
         #     _       _       _  _____           _           _
         #    | | ___ (_)_ __ | ||_   _| __ __ _ (_) ___  ___| |_ ___  _ __ _   _
@@ -132,73 +258,7 @@ if __name__ == "__main__":
         # \___/ \___/|_|_| |_|\__||_||_|  \__,_|/ |\___|\___|\__\___/|_|   \__, |
         #                                     |__/                         |___/
 
-        rv = parameters_in_file.ROSParameterVisitor(['trajectory_msgs.msg.JointTrajectory','JointTrajectory'],\
-                                                     limits_dict={}, msg_fields_dict={})
-        rv.visit(ast_file)
-
-        # Find new joints with kdl retargetor
-        # joint_names
-        # points, positions
-        synthesis_logger.debug("All variables: {0}".format(rv.scopes[0]))
-        synthesis_logger.debug("Parameters of interest: {0}".format(rv.msg_fields_dict))
-
-        if 'joint_names' in rv.msg_fields_dict.keys():
-            for joint_names in rv.msg_fields_dict['joint_names']:
-
-                if 'points' in rv.msg_fields_dict.keys() and \
-                   'positions' in rv.msg_fields_dict['points'].keys():
-
-                    # get values of joint_names
-                    if isinstance(joint_names,str):
-                        joints_names_values = rv.scopes[0].find(joint_names)
-                    else:
-                        joints_names_values = joint_names
-
-                    # if we have both joint_name fields and points/positions field
-                    for positions in rv.msg_fields_dict['points']['positions']:
-
-                        # get values of positions
-                        if isinstance(positions,str):
-                            positions_values = rv.scopes[0].find(positions)
-                        else:
-                            positions_values = positions
-
-                        # ensure joints_names_values and positions_values are the same length
-                        if len(joints_names_values) == len(positions_values):
-                            synthesis_logger.info("================ Joint Trajectory Replacement ================")
-                            synthesis_logger.info("joints_names_values:{0}".format(joints_names_values))
-                            synthesis_logger.info("positions_values:{0}".format(positions_values))
-
-                            for joint_list in joints_names_values:
-                                for pos_list in positions_values:
-                                    # do optimization with kdl_retargeter
-                                    # E.g: joints_names_values:[['j2n6s300_joint_1', 'j2n6s300_joint_2', 'j2n6s300_joint_3',\
-                                    #                       'j2n6s300_joint_4', 'j2n6s300_joint_5', 'j2n6s300_joint_6']]
-                                    # E.g: positions_values:[[2.2, 2.3, 1.57, 0, 0, 0]]
-                                    target_joints, best_ret_angles = process_urdf.find_ret_angles_from_source_joints(\
-                                        source_robot_name, target_robot_name, joint_list, pos_list, mode='scale_by_length')
-
-                                    # replace joint names
-                                    #[attribute_list, limit_violation, limit, cur_value, var_name]
-                                    replacement_list = [(['joint_names'], '--', \
-                                                        target_joints, joint_list, joint_names)]
-                                    replacement_with_redbaron.replace_parameters(red_obj, replacement_list)
-
-                                    # replace joint values
-                                    replacement_list = [(['points','positions'], '--', \
-                                                        best_ret_angles, pos_list, positions)]
-                                    replacement_with_redbaron.replace_parameters(red_obj, replacement_list)
-
-        """
-        #[attribute_list, limit_violation, limit, cur_value, var_name]
-        replacement_list = [(['joint_names'], '--', \
-                             ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', \
-                             'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'], \
-                             ['j2n6s300_joint_1', 'j2n6s300_joint_2', 'j2n6s300_joint_3', \
-                             'j2n6s300_joint_4', 'j2n6s300_joint_5', 'j2n6s300_joint_6'], 'JOINT_NAMES')]
-
-        replacement_with_redbaron.replace_parameters(red_obj, replacement_list)
-        """
+        convert_joint_traj_commands(red_obj, ast_file, source_robot_name, target_robot_name)
 
     # save to file
     replacement_with_redbaron.save_redbardon_obj_to_file(red_obj,dest_file)
